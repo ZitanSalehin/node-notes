6658a35a6975a0af8ad90e0ab9a971e5148c160e
Zitan Salehin <zitan-74@dipti.com.bd>
2024-05-07 17:33:42 +0600

    custom module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // console.log(global);
 const path = require('path');
 const os = require('os');
+const {add,substract,multiply,divide} = require('./basicOperation');
 
 // console.log(os);
 // console.log(os.type());
@@ -10,10 +11,14 @@ const os = require('os');
 // console.log(os.homedir());
 // console.log(__dirname);
 // console.log(__filename);
-const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
-const obj = JSON.parse(jsonString);
-console.log(obj);
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+// const obj = JSON.parse(jsonString);
+// console.log(obj);
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+console.log(add(10,20));
+console.log(substract(60,20));
+console.log(multiply(10,20));
+console.log(divide(40,20));
 
