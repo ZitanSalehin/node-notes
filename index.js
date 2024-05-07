7d6969c0afc94cdcdd6c367a62ad99c10458e762
Zitan Salehin <zitan-74@dipti.com.bd>
2024-05-07 17:28:20 +0600

    builtin module path os

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // var server = require('./nodeServer');
-var nodeServerWithPages = require('./nodeServerWithPages');
+// var nodeServerWithPages = require('./nodeServerWithPages');
+// console.log(global);
+const path = require('path');
+const os = require('os');
+
+// console.log(os);
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+// console.log(__dirname);
+// console.log(__filename);
+const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+const obj = JSON.parse(jsonString);
+console.log(obj);
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
 
